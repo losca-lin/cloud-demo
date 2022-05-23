75c0d40d55e04dce666a54535685d365e2513a47
losca <783840358@qq.com>
2022-05-23 14:59:48 +0800

    nacos注册中心

--- user-service/src/main/java/cn/itcast/user/UserApplication.java
+++ user-service/src/main/java/cn/itcast/user/UserApplication.java
@@ -1,13 +1,13 @@
 package cn.itcast.user;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @MapperScan("cn.itcast.user.mapper")
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class UserApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserApplication.class, args);
